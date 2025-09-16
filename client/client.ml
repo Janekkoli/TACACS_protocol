04083d89f169297223a2b73660df1cdae63fd09c
Janek Kolipinski Laptop <kolipinskijan@gmail.com>
2025-09-17 01:25:59 +0200

    Login in client

--- client/client.ml
+++ client/client.ml
@@ -1,12 +1,15 @@
 open Unix
 open Tacacs_extracted
+open Functions
 
-let string_to_char_list s =
-  let rec aux i acc =
-    if i < 0 then acc
-    else aux (i-1) (s.[i] :: acc)
-  in
-  aux (String.length s - 1) []
+let input_with_message(s) = 
+  print_string s;
+  flush Stdlib.stdout;
+  try
+    read_line ()
+  with End_of_file ->
+    print_endline "\nKoniec wejścia – program kończy działanie.";
+    exit 1
 
 let () =
   let argc = Array.length Sys.argv in
@@ -16,20 +19,21 @@ let () =
     let server_ip = Sys.argv.(1) in
     let port = int_of_string Sys.argv.(2) in
   
+  let user = input_with_message "Podaj nazwe uzytkownika: " in
+  let password = input_with_message "Podaj haslo: " in
   let sock = socket PF_INET SOCK_STREAM 0 in
   connect sock (ADDR_INET (inet_addr_of_string server_ip, port));
   Printf.printf "Connected to %s:%d\n%!" server_ip port;
 
   let auth_data = {
-    Auth.username = string_to_char_list "alice";
-    Auth.password = string_to_char_list "secret123";
+    Auth.username = string_to_char_list user;
+    Auth.password = string_to_char_list password;
     Auth.line =  12368;
     Auth.style = string_to_char_list "ascii";
   } in
 
   let package = encode_request(Auth auth_data) in
-  let package_string = (String.concat "" (List.map (String.make 1) package)) in
-  Printf.printf "Encoded package: %s\n%!" package_string;
+  let package_string = char_list_to_string package in
 
   ignore (write sock (Bytes.of_string package_string) 0 (String.length package_string));
   let buf = Bytes.create 1024 in
