4cdc548aeb01d7cb319799d0037d812a1672568b
Janek Kolipinski Laptop <kolipinskijan@gmail.com>
2025-09-17 20:15:52 +0200

    sending request from client and reciving response

--- client/client.ml
+++ client/client.ml
@@ -2,6 +2,16 @@ open Unix
 open Tacacs_extracted
 open Functions
 
+let send_request (req : request) (sock : file_descr) (server_ip : string) (port : int) : response option =
+  connect sock (ADDR_INET (inet_addr_of_string server_ip, port));
+  let package_string = char_list_to_string (encode_request req) in
+  ignore (write sock (Bytes.of_string package_string) 0 (String.length package_string)); (*TODO here should not be ignore but i dont know yet what should be*)
+  let buf = Bytes.create 1024 in
+  match read sock buf 0 1024, close sock with
+  | 0, _ -> None
+  | n ,_ -> parse_response (string_to_char_list (Bytes.sub_string buf 0 n))
+
+
 let input_with_message(s) = 
   print_string s;
   flush Stdlib.stdout;
@@ -22,9 +32,7 @@ let () =
   let user = input_with_message "Podaj nazwe uzytkownika: " in
   let password = input_with_message "Podaj haslo: " in
   let sock = socket PF_INET SOCK_STREAM 0 in
-  connect sock (ADDR_INET (inet_addr_of_string server_ip, port));
-  Printf.printf "Connected to %s:%d\n%!" server_ip port;
-
+  
   let auth_data = {
     Auth.username = string_to_char_list user;
     Auth.password = string_to_char_list password;
@@ -32,11 +40,8 @@ let () =
     Auth.style = string_to_char_list "ascii";
   } in
 
-  let package = encode_request(Auth auth_data) in
-  let package_string = char_list_to_string package in
-
-  ignore (write sock (Bytes.of_string package_string) 0 (String.length package_string));
-  let buf = Bytes.create 1024 in
-  let n = read sock buf 0 1024 in
-  if n > 0 then Printf.printf "Received echo: %s\n%!" (Bytes.sub_string buf 0 n);
-  close sock
+  let resp = send_request (Auth auth_data)  sock server_ip port in
+  match resp with
+  |None -> Printf.printf ":()"
+  | Some resp -> let resp_string = char_list_to_string (encode_response resp) in
+  Printf.printf "recived: %s\n" resp_string
